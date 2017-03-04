da131ccaffab30c43928cfa378004db5d50f24ba
Bernat Borrás-Paronella Petit <bernatbor15@gmail.com>
2017-03-04 12:02:05 +0100

    Add random button

--- sample/src/main/java/com/alorma/cityview/sample/MainActivity.java
+++ sample/src/main/java/com/alorma/cityview/sample/MainActivity.java
@@ -1,13 +1,26 @@
 package com.alorma.cityview.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import com.alorma.cityview.CityView;
 
 public class MainActivity extends AppCompatActivity {
+
+  private CityView cityView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    cityView = (CityView) findViewById(R.id.cityView);
+
+    findViewById(R.id.random).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        cityView.requestLayout();
+      }
+    });
   }
 }
